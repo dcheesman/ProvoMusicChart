dc841f8fc2ac26f0ef97900fa819e0bd28e925c4
Dean Cheemsan <dcheesman@gmail.com>
2014-06-07 23:29:50 -0600

    Deleted the arrows from the links

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -26,20 +26,6 @@ d3.json("/assets/data/band_info.json", function (band_data){
       .attr("width", width)
       .attr("height", height);
 
-  // Per-type markers, as they don't inherit styles.
-  svg.append("defs").selectAll("marker")
-      .data(["current_member_of", "related_to", "previous_member_of", "producer_for"])
-    .enter().append("marker")
-      .attr("id", function(d) { return d; })
-      .attr("viewBox", "0 -5 10 10")
-      .attr("refX", 15)
-      .attr("refY", -1.5)
-      .attr("markerWidth", 6)
-      .attr("markerHeight", 6)
-      .attr("orient", "auto")
-    .append("path")
-      .attr("d", "M0,-5L10,0L0,5");
-
   var path = svg.append("g").selectAll("path")
       .data(force.links())
     .enter().append("path")
